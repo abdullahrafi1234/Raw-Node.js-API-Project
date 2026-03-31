07801b208f77c7fcff2df98565c46f513f4dbb29
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2026-03-31 18:11:32 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// PROJECT NAME: Uptime Monitoring Application
+// DESCRIPTION:  A RestFul API to monitor up or down time of user defined links
+// CREATED BY:   Abdullah Al Rafi Bhuiyan
+// DATE:         30/03/2026
+
+// dependencies
+
+// const http = require('http')
+const http = require("http");
+
+// app object - module scaffolding
+const app = {};
+
+// configuration
+app.config = {};
+
+//create server
+app.createServer = () => {
+  const server = http.createServer();
+};
