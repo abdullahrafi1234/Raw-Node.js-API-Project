2d397a02bb1dcef958533e0b884cd0aad6254b64
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2026-04-06 13:01:59 +0600

    refactor done

---
+++ helpers/handleReqRes.js
@@ -0,0 +1,39 @@
+// PROJECT NAME: Handle Request Response
+// DESCRIPTION:  Handle Req and Res
+// CREATED BY:   Abdullah Al Rafi Bhuiyan
+// DATE:         06/04/2026
+
+// dependencies
+const { StringDecoder } = require("string_decoder");
+const url = require("url");
+
+const handler = {};
+
+handler.handleReqRes = (req, res) => {
+  // request handling
+
+  // get the url and parse it
+  const parsedUrl = url.parse(req.url, true);
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, "");
+  const method = req.method.toLowerCase();
+  const queryStringObject = parsedUrl.query;
+  const headersObject = req.headers;
+
+  const decoder = new StringDecoder("utf-8");
+  let realData = "";
+
+  req.on("data", (buffer) => {
+    realData += decoder.write(buffer);
+  });
+
+  req.on("end", () => {
+    realData += decoder.end();
+    console.log(realData);
+
+    // response handle
+    res.end("Hello Programmers");
+  });
+};
+
+module.exports = handler;
