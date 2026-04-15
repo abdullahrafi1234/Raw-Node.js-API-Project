1f32c2f05083c503c93f438e8cedc8ea1d25b422
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2026-04-15 12:20:46 +0600

    post check

--- handlers/routeHandlers/checkHandler.js
+++ handlers/routeHandlers/checkHandler.js
@@ -5,7 +5,9 @@
 
 // dependencies
 const data = require("../../lib/data");
-const { hash, parseJSON } = require("../../helpers/utilities");
+const { parseJSON, createRandomString } = require("../../helpers/utilities");
+const tokenHandler = require("../../handlers/routeHandlers/tokenHandler");
+const { maxChecks } = require("../../helpers/environments");
 
 // module scaffolding
 const handler = {};
@@ -13,24 +15,146 @@ const handler = {};
 handler.checkHandler = (requestProperties, callback) => {
   const acceptedMethods = ["get", "post", "put", "delete"];
   if (acceptedMethods.indexOf(requestProperties.method) > -1) {
-    handler._users[requestProperties.method](requestProperties, callback);
+    handler._check[requestProperties.method](requestProperties, callback);
     //
   } else {
     callback(405);
   }
 };
 
-handler._users = {};
+handler._check = {};
 
-handler._users.post = (requestProperties, callback) => {};
+handler._check.post = (requestProperties, callback) => {
+  let protocol =
+    typeof requestProperties.body.protocol === "string" &&
+    ["http", "https"].indexOf(requestProperties.body.protocol) > -1
+      ? requestProperties.body.protocol
+      : false;
+
+  let url =
+    typeof requestProperties.body.url === "string" &&
+    requestProperties.body.url.trim().length > 0
+      ? requestProperties.body.url
+      : false;
+
+  let method =
+    typeof requestProperties.body.method === "string" &&
+    ["get", "post", "put", "delete"].indexOf(requestProperties.body.method) > -1
+      ? requestProperties.body.method
+      : false;
+
+  let successCodes =
+    typeof requestProperties.body.successCodes === "object" &&
+    requestProperties.body.successCodes instanceof Array
+      ? requestProperties.body.successCodes
+      : false;
+
+  let timeoutSeconds =
+    typeof requestProperties.body.timeoutSeconds === "number" &&
+    requestProperties.body.timeoutSeconds % 1 === 0 &&
+    requestProperties.body.timeoutSeconds >= 1 &&
+    requestProperties.body.timeoutSeconds <= 5
+      ? requestProperties.body.timeoutSeconds
+      : false;
+
+  if (protocol && url && method && successCodes && timeoutSeconds) {
+    //verify token
+    const token =
+      typeof requestProperties.headersObject.token === "string"
+        ? requestProperties.headersObject.token
+        : false;
+
+    // lookup the user phone by reading the token
+    data.read("tokens", token, (err, tokenData) => {
+      if (!err && tokenData) {
+        let userPhone = parseJSON(tokenData).phone;
+
+        // lookup the user data
+        data.read("users", userPhone, (err, userData) => {
+          if (!err && userData) {
+            tokenHandler._token.verify(token, userPhone, (tokenIsValid) => {
+              if (tokenIsValid) {
+                let userObject = parseJSON(userData);
+                let userChecks =
+                  typeof userObject.checks === "object" &&
+                  userObject.checks instanceof Array
+                    ? userObject.checks
+                    : [];
+
+                if (userChecks.length < maxChecks) {
+                  //
+                  let checkId = createRandomString(20);
+                  let checkObject = {
+                    id: checkId,
+                    userPhone,
+                    protocol,
+                    url,
+                    method,
+                    successCodes,
+                    timeoutSeconds,
+                  };
+
+                  // save the object
+                  data.create("checks", checkId, checkObject, (err) => {
+                    if (!err) {
+                      // add check id to the users object
+                      userObject.checks = userChecks;
+                      userObject.checks.push(checkId);
+
+                      // save the new user data
+                      data.update("users", userPhone, userObject, (err) => {
+                        if (!err) {
+                          // return the data about the new check
+                          callback(200, checkObject);
+                        } else {
+                          callback(500, {
+                            error: "There was a problem in the server side",
+                          });
+                        }
+                      });
+                    } else {
+                      callback(500, {
+                        error: "There was a problem in the server side",
+                      });
+                    }
+                  });
+                } else {
+                  callback(401, {
+                    error: "User already reached max check limit",
+                  });
+                }
+              } else {
+                callback(403, {
+                  error: "Authentication error",
+                });
+              }
+            });
+          } else {
+            callback(403, {
+              error: "user not found",
+            });
+          }
+        });
+      } else {
+        callback(403, {
+          error: "Authentication problem",
+        });
+      }
+    });
+  } else {
+    callback(400, {
+      error: "You have a problem in your request",
+    });
+  }
+};
 
 // Authentication
-handler._users.get = (requestProperties, callback) => {};
+handler._check.get = (requestProperties, callback) => {};
 
 // Authentication
-handler._users.put = (requestProperties, callback) => {};
+handler._check.put = (requestProperties, callback) => {};
 
 // Authentication
-handler._users.delete = (requestProperties, callback) => {};
+handler._check.delete = (requestProperties, callback) => {};
 
 module.exports = handler;
